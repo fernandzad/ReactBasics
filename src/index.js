3206690affeba6f1af2f9970b85c4db6f5047412
Adrian Fernandez <ing.adrian94fdz@gmail.com>
2020-01-20 11:19:48 -0600

    Getting resources from the fitness API using async await

--- src/index.js
+++ src/index.js
@@ -62,6 +62,7 @@
 //imgUrl: https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-256.png
 // https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-256.png
 //https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06
+// Colores de prueba gradientes: #E033FF - #FF3377
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
